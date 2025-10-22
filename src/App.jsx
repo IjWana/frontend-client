fd8e69a297d0fd44ee552f00b566a37746a974fd
Ijwana <raymondijoh@gmail.com>
2025-10-22 15:52:13 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Dashboard from "./pages/Dashboard";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
@@ -11,6 +12,16 @@ export default function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
+
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={false}
+      closeOnClick
+      newestOnTop
+      pauseOnHover
+      draggable
+    />
     </>
   )
 }
